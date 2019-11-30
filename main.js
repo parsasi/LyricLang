4040e878e21dcc66dee055631763293e009de26e
Parsa Safavi <parsasi@rokcetmail.com>
2019-11-30 14:20:46 -0800

    lyrics are added

--- main.js
+++ main.js
@@ -2,20 +2,399 @@ let songs = [{
     id : '1',
     name : 'Yellow Submarine',
     address : 'songs/song1.mp3',
-    eng : `` , 
-    mand : ``
+    eng : 
+`In the town where I was born
+Lived a man who sailed to sea
+And he told us of his life
+In the land of submarines
+So we sailed up to the sun
+'Til we found a sea of green
+And we lived beneath the waves
+In our yellow submarine
+We all live in a yellow submarine
+Yellow submarine, yellow submarine
+We all live in a yellow submarine
+Yellow submarine, yellow submarine
+And our friends are all aboard
+Many more of them live next door
+And the band begins to play
+We all live in a yellow submarine
+Yellow submarine, yellow submarine
+We all live in a yellow submarine
+Yellow submarine, yellow submarine
+As we live a life of ease
+Everyone of us has all we need
+Sky of blue and sea of green
+In our yellow submarine
+We all live in a yellow submarine
+Yellow submarine, yellow submarine
+We all live in a yellow submarine
+Yellow submarine, yellow submarine
+We all live in a yellow submarine
+Yellow submarine, yellow submarine
+We all live in a yellow submarine
+Yellow submarine, yellow submarine` , 
+    mand : 
+`在我出生的城镇
+住过一个出海的人
+他告诉我们他的生活
+在潜艇之地
+所以我们驶向太阳
+直到我们发现一片绿色的海洋
+我们住在海浪下
+在我们的黄色潜水艇中
+我们都住在黄色潜水艇
+黄潜艇黄潜艇
+我们都住在黄色潜水艇
+黄潜艇黄潜艇
+我们的朋友都在船上
+许多人住在隔壁
+乐队开始演奏
+我们都住在黄色潜水艇
+黄潜艇黄潜艇
+我们都住在黄色潜水艇
+黄潜艇黄潜艇
+当我们过着轻松的生活
+我们每个人都有我们需要的一切
+蓝色的天空和绿色的海洋
+在我们的黄色潜水艇中
+我们都住在黄色潜水艇
+黄潜艇黄潜艇
+我们都住在黄色潜水艇
+黄潜艇黄潜艇
+我们都住在黄色潜水艇
+黄潜艇黄潜艇
+我们都住在黄色潜水艇
+黄潜艇黄潜艇`
 } , {
     id : '2',
     name : 'Jingle Bells',
     address : 'songs/song2.mp3',
-    eng : `` , 
-    mand : ``
+    eng : 
+`Dashing through the snow
+In a one-horse open sleigh
+O'er the fields we go
+Laughing all the way
+Bells on bobtails ring
+Making spirits bright
+What fun it is to ride and sing
+A sleighing song tonight
+Jingle bells, jingle bells
+Jingle all the way
+Oh, what fun it is to ride
+In a one-horse open sleigh, hey
+Jingle bells, jingle bells
+Jingle all the way
+Oh, what fun it is to ride
+In a one-horse open sleigh
+A day or two ago
+I thought I'd take a ride
+And soon, Miss Fanny Bright
+Was seated by my side
+The horse was lean and lank
+Misfortune seemed his lot
+He got into a drifted bank
+And then we got upsot
+Jingle bells, jingle bells
+Jingle all the way
+Oh, what fun it is to ride
+In a one-horse open sleigh, hey
+Jingle bells, jingle bells
+Jingle all the way
+Oh, what fun it is to ride
+In a one-horse open sleigh
+A day or two ago
+The story I must tell
+I went out on the snow
+And on my back I fell
+A gent was riding by
+In a one-horse open sleigh
+He laughed as there I sprawling lie
+But quickly drove away
+Jingle bells, jingle bells
+Jingle all the way
+Oh, what fun it is to ride
+In a one-horse open sleigh, hey
+Jingle bells, jingle bells
+Jingle all the way
+Oh, what fun it is to ride
+In a one-horse open sleigh
+Now the ground is white
+Go it while you're young
+Take the girls tonight
+And sing this sleighing song
+Just get a bobtailed bay
+Two forty as his speed
+Hitch him to an open sleigh
+And crack, you'll take the lead
+Jingle bells, jingle bells
+Jingle all the way
+Oh, what fun it is to ride
+In a one-horse open sleigh, hey
+Jingle bells, jingle bells
+Jingle all the way
+Oh, what fun it is to ride
+In a one-horse open sleigh` , 
+    mand : 
+`在雪地里奔跑
+在一匹开马的雪橇上
+我们去的田野
+一路大笑
+短尾铃上的铃铛
+使精神焕发
+骑和唱歌有什么乐趣
+今晚的雪橇歌
+铃儿响叮当，铃儿响叮当
+一路叮当
+哦，骑车有什么乐趣
+在一匹马的雪橇中，嘿
+铃儿响叮当，铃儿响叮当
+一路叮当
+哦，骑车有什么乐趣
+在一匹开马的雪橇上
+一两天前
+我以为我会骑
+很快，范妮·布莱特小姐
+坐在我身边
+这匹马瘦而瘦
+不幸似乎是他的很多
+他进了一家漂流的银行
+然后我们快了
+铃儿响叮当，铃儿响叮当
+一路叮当
+哦，骑车有什么乐趣
+在一匹马的雪橇中，嘿
+铃儿响叮当，铃儿响叮当
+一路叮当
+哦，骑车有什么乐趣
+在一匹开马的雪橇上
+一两天前
+我必须讲的故事
+我在雪地上出去
+在我的背上，我跌倒了
+一个绅士骑着马
+在一匹开马的雪橇上
+我撒谎时他笑了
+但是很快就开走了
+铃儿响叮当，铃儿响叮当
+一路叮当
+哦，骑车有什么乐趣
+在一匹马的雪橇中，嘿
+铃儿响叮当，铃儿响叮当
+一路叮当
+哦，骑车有什么乐趣
+在一匹开马的雪橇上
+现在地面是白色的
+年轻的时候去吧
+今晚带女孩
+唱这首雪橇歌
+刚被缠结的海湾
+他的速度是四十
+把他拴在一个开放的雪橇上
+破解，您将带头
+铃儿响叮当，铃儿响叮当
+一路叮当
+哦，骑车有什么乐趣
+在一匹马的雪橇中，嘿
+铃儿响叮当，铃儿响叮当
+一路叮当
+哦，骑车有什么乐趣
+在一匹开马的雪橇上`
 } ,{
     id : '3',
     name : 'Nights',
     address : 'songs/song2.mp3',
-    eng : `` , 
-    mand : ``
+    eng : 
+`Round your city, round the clock
+Everybody needs you
+No, you can't make everybody equal
+Although you got beaucoup family
+You don't even got nobody bein' honest with you
+Breathe 'til I evaporated
+My whole body see through
+Transportation, handmade
+And I know it better than most people
+I don't trust 'em anyways
+You can't break the law with them
+Get some gushy, have a calm night
+Shooters killin' left and right
+Workin' through your worst night
+If I get my money right
+You know I won't need you
+And I tell you,
+I hope the sack is full up
+I'm fuckin', no, I'm fucked up
+Spend it when I get that
+I ain't tryna keep you
+Can't keep up a conversation
+Can't nobody reach you
+Why your eyes well up?
+Did you call me from a séance?
+You are from my past life
+Hope you're doin' well, bruh
+I been out here head first
+Always like the head first
+Signal comin' in and out
+Hope you're doin' well, bruh
+Everybody needs you
+Everybody needs you
+Ooh, nani nani
+This feel like a quaalude
+No sleep in my body
+Ain't no bitch in my body
+New beginning's, ah
+New beginning's, wake up akh
+The sun's goin' down
+Time to start your day, bruh
+Can't keep bein' laid off
+Know you need the money if you gon' survive
+The every night shit
+Every day shit
+Droppin' baby off at home before my night shift
+You know I can't hear none of that spend the night shit
+That kumbaya shit
+Wanna see nirvana, but don't wanna die yet
+Wanna feel that na na though, could you come by?
+Fuck with me after my shift
+Know them boys wanna see me broke down and shit
+Bummed out and shit, stressed out and shit
+That's every day shit
+Shut the fuck up, I don't want your conversation
+Rollin' marijuana, that's a cheap vacation
+My every day shit, every night shit, my every day shit
+All my night, been ready for you all my night
+Been waitin' on you all my night
+I'll buzz you in, just let me know when you outside
+All my night, you been missin' all my night
+Still got some good nights memorized
+And the look back's gettin' me right
+Every night fucks every day up
+Every day patches the night up
+On God, you should match it, it's that KO
+No white lighters 'til I fuck my 28th up
+1998, my family had that Acura
+Oh, the Legend
+Kept at least six discs in the changer
+Back when Boswell and Percy had it active
+Couple bishops in the city buildin' mansions
+All the reverends
+Preachin' self made millionaire status
+When we could only eat at Shoney's on occasion
+After 'Trina hit I had to transfer campus
+Your apartment out in Houston's where I waited
+Stayin' with you when I didn't have a address
+Fuckin' on you when I didn't own a mattress
+Workin' on a way to make it outta Texas, every night
+Droppin' baby off at home before my night shift, yeah
+You know I can't hear none of that spend the night shit, kumbaya shit
+Want to see nirvana, but don't wanna die yet
+Wanna feel that na na though, could you come by?
+Fuck with me after my shift
+Know them boys wanna see me broke down
+See me bummed out, stressed out
+That's just everyday shit
+Shut the fuck up, I don't want your conversation
+Rollin' marijuana, that's a cheap vacation
+My every day shit, my every day shit
+My every night shit, my every day shit
+My every day shit, my every day shit
+My every day shit, my every night shit
+` , 
+    mand : 
+`昼夜不停地游览城市
+每个人都需要你
+不，你不能使所有人平等
+虽然你有美丽的家庭
+你甚至没有人对你诚实
+呼吸直到我蒸发
+我整个身体都看穿了
+手工运输
+我比大多数人都了解
+反正我也不信任他们
+你不能和他们违法
+快点，晚上安静
+射手杀死左右
+渡过最糟糕的夜晚
+如果我把钱弄对了
+你知道我不需要你
+我告诉你，
+我希望麻袋装满
+我他妈的，不，我他妈的
+当我得到它时花
+我不是想让你
+无法跟上对话
+没人能联系到你
+为什么你的眼睛好起来？
+你是不是从séance打电话给我的？
+你来自我前世
+希望你一切都好，布鲁
+我先去了这里
+总是喜欢头
+信号进出
+希望你一切都好，布鲁
+每个人都需要你
+每个人都需要你
+哦，娜妮娜妮
+这感觉像是一个quaalude
+我的身体没有睡眠
+我的身体不是bit子
+新的开始啊
+新的开始，唤醒akh
+太阳下​​山了
+是时候开始新的一天了，布鲁
+无法保持裁员
+知道要生存就需要钱
+每天晚上屎
+每天屎
+在夜班前将宝贝放下在家
+你知道我听不见那些花了整夜的时间
+那个昆巴耶屎
+想看必杀技，但不想死
+想要感到自己不行，你能过去吗？
+轮班后跟我他妈的
+认识他们，男孩们想见我崩溃了，拉屎
+筋疲力尽，拉屎，压力加大，拉屎
+每天都这样
+闭嘴，我不要你的谈话
+Rollin'大麻，那是一个便宜的假期
+我每天的屎，每天晚上的屎，我每天的屎
+我一整夜都准备为你整夜
+一整夜都在等你
+我会嗡嗡叫你，只要你在外面就让我知道
+我整晚，你整晚都在想念
+还记得一些美好的夜晚
+然后回头看我就对了
+每天晚上都乱搞
+每天修补夜晚
+在上帝上，你应该匹配它，那就是
+没有白色打火机，直到我他妈的我的28岁以上
+1998年，我家有那个Ac歌
+哦，传奇
+更换器中至少保留了六张光盘
+当Boswell和Percy活跃起来时
+这座城市的夫妇主教在大厦
+所有牧师
+传教士的百万富翁身份
+有时候我们只能在肖尼家吃饭
+“特里娜”袭击后，我不得不转校
+你在休斯顿等我的公寓
+当我没有地址时待在你身边
+当我没有床垫时，该死在你身上
+每天晚上都在努力使它远离德克萨斯州
+在我夜班前将宝贝放下在家，是的
+你知道我听不见夜宵，昆巴耶屎
+想看必杀技，但不想死
+想要感到自己不行，你能过去吗？
+轮班后跟我他妈的
+认识他们，男孩想见我崩溃
+看到我很沮丧，压力很大
+那只是每天的屎
+闭嘴，我不要你的谈话
+Rollin'大麻，那是一个便宜的假期
+我每天都屎，我每天都屎
+我每天晚上屎，我每天屎
+我每天都屎，我每天都屎
+我每天都屎，我每天晚上都屎`
 }];
 
 selectRandomSongs = () => {
@@ -23,5 +402,3 @@ selectRandomSongs = () => {
     max = Math.floor(songs.length -1);
     return songs[Math.floor(Math.random() * (max - min + 1)) + min]; 
 }
-
-console.log(selectRandomSongs());
