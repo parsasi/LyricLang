52f2df664c0c6e5596a7f481fefa080f4b5bdbf8
Parsa Safavi <parsasi@rokcetmail.com>
2019-11-30 12:54:24 -0800

    Basic Data strcture

---
+++ main.js
@@ -0,0 +1,26 @@
+let songs = [{
+    id : '1',
+    name : 'Yellow Submarine',
+    address : 'songs/song1.mp3',
+    eng : `` , 
+    mand : ``
+} , {
+    id : '2',
+    name : 'Jingle Bells',
+    address : 'songs/song2.mp3',
+    eng : `` , 
+    mand : ``
+} ,{
+    id : '3',
+    name : 'Nights',
+    address : 'songs/song2.mp3',
+    eng : `` , 
+    mand : ``
+}];
+
+selectRandomSongs = () => {
+    min = Math.ceil(0);
+    max = Math.floor(songs.length -1);
+    return Math.floor(Math.random() * (max - min + 1)) + min; 
+}
+
