e2375beb8ccf430fc054607d9551a29ffd7a0e5c
Don <donli90@hotmail.com>
2019-11-30 17:14:05 -0800

    abc

--- main.js
+++ main.js
@@ -4,397 +4,397 @@ let songs = [{
     address : 'songs/song1.mp3',
     eng : 
 `In the town where I was born
-Lived a man who sailed to sea
-And he told us of his life
-In the land of submarines
-So we sailed up to the sun
-'Til we found a sea of green
-And we lived beneath the waves
-In our yellow submarine
-We all live in a yellow submarine
-Yellow submarine, yellow submarine
-We all live in a yellow submarine
-Yellow submarine, yellow submarine
-And our friends are all aboard
-Many more of them live next door
-And the band begins to play
-We all live in a yellow submarine
-Yellow submarine, yellow submarine
-We all live in a yellow submarine
-Yellow submarine, yellow submarine
-As we live a life of ease
-Everyone of us has all we need
-Sky of blue and sea of green
-In our yellow submarine
-We all live in a yellow submarine
-Yellow submarine, yellow submarine
-We all live in a yellow submarine
-Yellow submarine, yellow submarine
-We all live in a yellow submarine
-Yellow submarine, yellow submarine
-We all live in a yellow submarine
-Yellow submarine, yellow submarine` , 
+\nLived a man who sailed to sea
+\nAnd he told us of his life
+\nIn the land of submarines
+\nSo we sailed up to the sun
+\n'Til we found a sea of green
+\nAnd we lived beneath the waves
+\nIn our yellow submarine
+\nWe all live in a yellow submarine
+\nYellow submarine, yellow submarine
+\nWe all live in a yellow submarine
+\nYellow submarine, yellow submarine
+\nAnd our friends are all aboard
+\nMany more of them live next door
+\nAnd the band begins to play
+\nWe all live in a yellow submarine
+\nYellow submarine, yellow submarine
+\nWe all live in a yellow submarine
+\nYellow submarine, yellow submarine
+\nAs we live a life of ease
+\nEveryone of us has all we need
+\nSky of blue and sea of green
+\nIn our yellow submarine
+\nWe all live in a yellow submarine
+\nYellow submarine, yellow submarine
+\nWe all live in a yellow submarine
+\nYellow submarine, yellow submarine
+\nWe all live in a yellow submarine
+\nYellow submarine, yellow submarine
+\nWe all live in a yellow submarine
+\nYellow submarine, yellow submarine` , 
     mand : 
 `在我出生的城镇
-住过一个出海的人
-他告诉我们他的生活
-在潜艇之地
-所以我们驶向太阳
-直到我们发现一片绿色的海洋
-我们住在海浪下
-在我们的黄色潜水艇中
-我们都住在黄色潜水艇
-黄潜艇黄潜艇
-我们都住在黄色潜水艇
-黄潜艇黄潜艇
-我们的朋友都在船上
-许多人住在隔壁
-乐队开始演奏
-我们都住在黄色潜水艇
-黄潜艇黄潜艇
-我们都住在黄色潜水艇
-黄潜艇黄潜艇
-当我们过着轻松的生活
-我们每个人都有我们需要的一切
-蓝色的天空和绿色的海洋
-在我们的黄色潜水艇中
-我们都住在黄色潜水艇
-黄潜艇黄潜艇
-我们都住在黄色潜水艇
-黄潜艇黄潜艇
-我们都住在黄色潜水艇
-黄潜艇黄潜艇
-我们都住在黄色潜水艇
-黄潜艇黄潜艇`
+\n住过一个出海的人
+\n他告诉我们他的生活
+\n在潜艇之地
+\n所以我们驶向太阳
+\n直到我们发现一片绿色的海洋
+\n我们住在海浪下
+\n在我们的黄色潜水艇中
+\n我们都住在黄色潜水艇
+\n黄潜艇黄潜艇
+\n我们都住在黄色潜水艇
+\n黄潜艇黄潜艇
+\n我们的朋友都在船上
+\n许多人住在隔壁
+\n乐队开始演奏
+\n我们都住在黄色潜水艇
+\n黄潜艇黄潜艇
+\n我们都住在黄色潜水艇
+\n黄潜艇黄潜艇
+\n当我们过着轻松的生活
+\n我们每个人都有我们需要的一切
+\n蓝色的天空和绿色的海洋
+\n在我们的黄色潜水艇中
+\n我们都住在黄色潜水艇
+\n黄潜艇黄潜艇
+\n我们都住在黄色潜水艇
+\n黄潜艇黄潜艇
+\n我们都住在黄色潜水艇
+\n黄潜艇黄潜艇
+\n我们都住在黄色潜水艇
+\n黄潜艇黄潜艇`
 } , {
     id : '2',
     name : 'Jingle Bells',
     address : 'songs/song2.mp3',
     eng : 
 `Dashing through the snow
-In a one-horse open sleigh
-O'er the fields we go
-Laughing all the way
-Bells on bobtails ring
-Making spirits bright
-What fun it is to ride and sing
-A sleighing song tonight
-Jingle bells, jingle bells
-Jingle all the way
-Oh, what fun it is to ride
-In a one-horse open sleigh, hey
-Jingle bells, jingle bells
-Jingle all the way
-Oh, what fun it is to ride
-In a one-horse open sleigh
-A day or two ago
-I thought I'd take a ride
-And soon, Miss Fanny Bright
-Was seated by my side
-The horse was lean and lank
-Misfortune seemed his lot
-He got into a drifted bank
-And then we got upsot
-Jingle bells, jingle bells
-Jingle all the way
-Oh, what fun it is to ride
-In a one-horse open sleigh, hey
-Jingle bells, jingle bells
-Jingle all the way
-Oh, what fun it is to ride
-In a one-horse open sleigh
-A day or two ago
-The story I must tell
-I went out on the snow
-And on my back I fell
-A gent was riding by
-In a one-horse open sleigh
-He laughed as there I sprawling lie
-But quickly drove away
-Jingle bells, jingle bells
-Jingle all the way
-Oh, what fun it is to ride
-In a one-horse open sleigh, hey
-Jingle bells, jingle bells
-Jingle all the way
-Oh, what fun it is to ride
-In a one-horse open sleigh
-Now the ground is white
-Go it while you're young
-Take the girls tonight
-And sing this sleighing song
-Just get a bobtailed bay
-Two forty as his speed
-Hitch him to an open sleigh
-And crack, you'll take the lead
-Jingle bells, jingle bells
-Jingle all the way
-Oh, what fun it is to ride
-In a one-horse open sleigh, hey
-Jingle bells, jingle bells
-Jingle all the way
-Oh, what fun it is to ride
-In a one-horse open sleigh` , 
+\nIn a one-horse open sleigh
+\nO'er the fields we go
+\nLaughing all the way
+\nBells on bobtails ring
+\nMaking spirits bright
+\nWhat fun it is to ride and sing
+\nA sleighing song tonight
+\nJingle bells, jingle bells
+\nJingle all the way
+\nOh, what fun it is to ride
+\nIn a one-horse open sleigh, hey
+\nJingle bells, jingle bells
+\nJingle all the way
+\nOh, what fun it is to ride
+\nIn a one-horse open sleigh
+\nA day or two ago
+\nI thought I'd take a ride
+\nAnd soon, Miss Fanny Bright
+\nWas seated by my side
+\nThe horse was lean and lank
+\nMisfortune seemed his lot
+\nHe got into a drifted bank
+\nAnd then we got upsot
+\nJingle bells, jingle bells
+\nJingle all the way
+\nOh, what fun it is to ride
+\nIn a one-horse open sleigh, hey
+\nJingle bells, jingle bells
+\nJingle all the way
+\nOh, what fun it is to ride
+\nIn a one-horse open sleigh
+\nA day or two ago
+\nThe story I must tell
+\nI went out on the snow
+\nAnd on my back I fell
+\nA gent was riding by
+\nIn a one-horse open sleigh
+\nHe laughed as there I sprawling lie
+\nBut quickly drove away
+\nJingle bells, jingle bells
+\nJingle all the way
+\nOh, what fun it is to ride
+\nIn a one-horse open sleigh, hey
+\nJingle bells, jingle bells
+\nJingle all the way
+\nOh, what fun it is to ride
+\nIn a one-horse open sleigh
+\nNow the ground is white
+\nGo it while you're young
+\nTake the girls tonight
+\nAnd sing this sleighing song
+\nJust get a bobtailed bay
+\nTwo forty as his speed
+\nHitch him to an open sleigh
+\nAnd crack, you'll take the lead
+\nJingle bells, jingle bells
+\nJingle all the way
+\nOh, what fun it is to ride
+\nIn a one-horse open sleigh, hey
+\nJingle bells, jingle bells
+\nJingle all the way
+\nOh, what fun it is to ride
+\nIn a one-horse open sleigh` , 
     mand : 
 `在雪地里奔跑
-在一匹开马的雪橇上
-我们去的田野
-一路大笑
-短尾铃上的铃铛
-使精神焕发
-骑和唱歌有什么乐趣
-今晚的雪橇歌
-铃儿响叮当，铃儿响叮当
-一路叮当
-哦，骑车有什么乐趣
-在一匹马的雪橇中，嘿
-铃儿响叮当，铃儿响叮当
-一路叮当
-哦，骑车有什么乐趣
-在一匹开马的雪橇上
-一两天前
-我以为我会骑
-很快，范妮·布莱特小姐
-坐在我身边
-这匹马瘦而瘦
-不幸似乎是他的很多
-他进了一家漂流的银行
-然后我们快了
-铃儿响叮当，铃儿响叮当
-一路叮当
-哦，骑车有什么乐趣
-在一匹马的雪橇中，嘿
-铃儿响叮当，铃儿响叮当
-一路叮当
-哦，骑车有什么乐趣
-在一匹开马的雪橇上
-一两天前
-我必须讲的故事
-我在雪地上出去
-在我的背上，我跌倒了
-一个绅士骑着马
-在一匹开马的雪橇上
-我撒谎时他笑了
-但是很快就开走了
-铃儿响叮当，铃儿响叮当
-一路叮当
-哦，骑车有什么乐趣
-在一匹马的雪橇中，嘿
-铃儿响叮当，铃儿响叮当
-一路叮当
-哦，骑车有什么乐趣
-在一匹开马的雪橇上
-现在地面是白色的
-年轻的时候去吧
-今晚带女孩
-唱这首雪橇歌
-刚被缠结的海湾
-他的速度是四十
-把他拴在一个开放的雪橇上
-破解，您将带头
-铃儿响叮当，铃儿响叮当
-一路叮当
-哦，骑车有什么乐趣
-在一匹马的雪橇中，嘿
-铃儿响叮当，铃儿响叮当
-一路叮当
-哦，骑车有什么乐趣
-在一匹开马的雪橇上`
+\n在一匹开马的雪橇上
+\n我们去的田野
+\n一路大笑
+\n短尾铃上的铃铛
+\n使精神焕发
+\n骑和唱歌有什么乐趣
+\n今晚的雪橇歌
+\n铃儿响叮当，铃儿响叮当
+\n一路叮当
+\n哦，骑车有什么乐趣
+\n在一匹马的雪橇中，嘿
+\n铃儿响叮当，铃儿响叮当
+\n一路叮当
+\n哦，骑车有什么乐趣
+\n在一匹开马的雪橇上
+\n一两天前
+\n我以为我会骑
+\n很快，范妮·布莱特小姐
+\n坐在我身边
+\n这匹马瘦而瘦
+\n不幸似乎是他的很多
+\n他进了一家漂流的银行
+\n然后我们快了
+\n铃儿响叮当，铃儿响叮当
+\n一路叮当
+\n哦，骑车有什么乐趣
+\n在一匹马的雪橇中，嘿
+\n铃儿响叮当，铃儿响叮当
+\n一路叮当
+\n哦，骑车有什么乐趣
+\n在一匹开马的雪橇上
+\n一两天前
+\n我必须讲的故事
+\n我在雪地上出去
+\n在我的背上，我跌倒了
+\n一个绅士骑着马
+\n在一匹开马的雪橇上
+\n我撒谎时他笑了
+\n但是很快就开走了
+\n铃儿响叮当，铃儿响叮当
+\n一路叮当
+\n哦，骑车有什么乐趣
+\n在一匹马的雪橇中，嘿
+\n铃儿响叮当，铃儿响叮当
+\n一路叮当
+\n哦，骑车有什么乐趣
+\n在一匹开马的雪橇上
+\n现在地面是白色的
+\n年轻的时候去吧
+\n今晚带女孩
+\n唱这首雪橇歌
+\n刚被缠结的海湾
+\n他的速度是四十
+\n把他拴在一个开放的雪橇上
+\n破解，您将带头
+\n铃儿响叮当，铃儿响叮当
+\n一路叮当
+\n哦，骑车有什么乐趣
+\n在一匹马的雪橇中，嘿
+\n铃儿响叮当，铃儿响叮当
+\n一路叮当
+\n哦，骑车有什么乐趣
+\n在一匹开马的雪橇上`
 } ,{
     id : '3',
     name : 'Nights',
     address : 'songs/song2.mp3',
     eng : 
 `Round your city, round the clock
-Everybody needs you
-No, you can't make everybody equal
-Although you got beaucoup family
-You don't even got nobody bein' honest with you
-Breathe 'til I evaporated
-My whole body see through
-Transportation, handmade
-And I know it better than most people
-I don't trust 'em anyways
-You can't break the law with them
-Get some gushy, have a calm night
-Shooters killin' left and right
-Workin' through your worst night
-If I get my money right
-You know I won't need you
-And I tell you,
-I hope the sack is full up
-I'm fuckin', no, I'm fucked up
-Spend it when I get that
-I ain't tryna keep you
-Can't keep up a conversation
-Can't nobody reach you
-Why your eyes well up?
-Did you call me from a séance?
-You are from my past life
-Hope you're doin' well, bruh
-I been out here head first
-Always like the head first
-Signal comin' in and out
-Hope you're doin' well, bruh
-Everybody needs you
-Everybody needs you
-Ooh, nani nani
-This feel like a quaalude
-No sleep in my body
-Ain't no bitch in my body
-New beginning's, ah
-New beginning's, wake up akh
-The sun's goin' down
-Time to start your day, bruh
-Can't keep bein' laid off
-Know you need the money if you gon' survive
-The every night shit
-Every day shit
-Droppin' baby off at home before my night shift
-You know I can't hear none of that spend the night shit
-That kumbaya shit
-Wanna see nirvana, but don't wanna die yet
-Wanna feel that na na though, could you come by?
-Fuck with me after my shift
-Know them boys wanna see me broke down and shit
-Bummed out and shit, stressed out and shit
-That's every day shit
-Shut the fuck up, I don't want your conversation
-Rollin' marijuana, that's a cheap vacation
-My every day shit, every night shit, my every day shit
-All my night, been ready for you all my night
-Been waitin' on you all my night
-I'll buzz you in, just let me know when you outside
-All my night, you been missin' all my night
-Still got some good nights memorized
-And the look back's gettin' me right
-Every night fucks every day up
-Every day patches the night up
-On God, you should match it, it's that KO
-No white lighters 'til I fuck my 28th up
-1998, my family had that Acura
-Oh, the Legend
-Kept at least six discs in the changer
-Back when Boswell and Percy had it active
-Couple bishops in the city buildin' mansions
-All the reverends
-Preachin' self made millionaire status
-When we could only eat at Shoney's on occasion
-After 'Trina hit I had to transfer campus
-Your apartment out in Houston's where I waited
-Stayin' with you when I didn't have a address
-Fuckin' on you when I didn't own a mattress
-Workin' on a way to make it outta Texas, every night
-Droppin' baby off at home before my night shift, yeah
-You know I can't hear none of that spend the night shit, kumbaya shit
-Want to see nirvana, but don't wanna die yet
-Wanna feel that na na though, could you come by?
-Fuck with me after my shift
-Know them boys wanna see me broke down
-See me bummed out, stressed out
-That's just everyday shit
-Shut the fuck up, I don't want your conversation
-Rollin' marijuana, that's a cheap vacation
-My every day shit, my every day shit
-My every night shit, my every day shit
-My every day shit, my every day shit
-My every day shit, my every night shit
+\nEverybody needs you
+\nNo, you can't make everybody equal
+\nAlthough you got beaucoup family
+\nYou don't even got nobody bein' honest with you
+\nBreathe 'til I evaporated
+\nMy whole body see through
+\nTransportation, handmade
+\nAnd I know it better than most people
+\nI don't trust 'em anyways
+\nYou can't break the law with them
+\nGet some gushy, have a calm night
+\nShooters killin' left and right
+\nWorkin' through your worst night
+\nIf I get my money right
+\nYou know I won't need you
+\nAnd I tell you,
+\nI hope the sack is full up
+\nI'm fuckin', no, I'm fucked up
+\nSpend it when I get that
+\nI ain't tryna keep you
+\nCan't keep up a conversation
+\nCan't nobody reach you
+\nWhy your eyes well up?
+\nDid you call me from a séance?
+\nYou are from my past life
+\nHope you're doin' well, bruh
+\nI been out here head first
+\nAlways like the head first
+\nSignal comin' in and out
+\nHope you're doin' well, bruh
+\nEverybody needs you
+\nEverybody needs you
+\nOoh, nani nani
+\nThis feel like a quaalude
+\nNo sleep in my body
+\nAin't no bitch in my body
+\nNew beginning's, ah
+\nNew beginning's, wake up akh
+\nThe sun's goin' down
+\nTime to start your day, bruh
+\nCan't keep bein' laid off
+\nKnow you need the money if you gon' survive
+\nThe every night shit
+\nEvery day shit
+\nDroppin' baby off at home before my night shift
+\nYou know I can't hear none of that spend the night shit
+\nThat kumbaya shit
+\nWanna see nirvana, but don't wanna die yet
+\nWanna feel that na na though, could you come by?
+\nFuck with me after my shift
+\nKnow them boys wanna see me broke down and shit
+\nBummed out and shit, stressed out and shit
+\nThat's every day shit
+\nShut the fuck up, I don't want your conversation
+\nRollin' marijuana, that's a cheap vacation
+\nMy every day shit, every night shit, my every day shit
+\nAll my night, been ready for you all my night
+\nBeen waitin' on you all my night
+\nI'll buzz you in, just let me know when you outside
+\nAll my night, you been missin' all my night
+\nStill got some good nights memorized
+\nAnd the look back's gettin' me right
+\nEvery night fucks every day up
+\nEvery day patches the night up
+\nOn God, you should match it, it's that KO
+\nNo white lighters 'til I fuck my 28th up
+\n1998, my family had that Acura
+\nOh, the Legend
+\nKept at least six discs in the changer
+\nBack when Boswell and Percy had it active
+\nCouple bishops in the city buildin' mansions
+\nAll the reverends
+\nPreachin' self made millionaire status
+\nWhen we could only eat at Shoney's on occasion
+\nAfter 'Trina hit I had to transfer campus
+\nYour apartment out in Houston's where I waited
+\nStayin' with you when I didn't have a address
+\nFuckin' on you when I didn't own a mattress
+\nWorkin' on a way to make it outta Texas, every night
+\nDroppin' baby off at home before my night shift, yeah
+\nYou know I can't hear none of that spend the night shit, kumbaya shit
+\nWant to see nirvana, but don't wanna die yet
+\nWanna feel that na na though, could you come by?
+\nFuck with me after my shift
+\nKnow them boys wanna see me broke down
+\nSee me bummed out, stressed out
+\nThat's just everyday shit
+\nShut the fuck up, I don't want your conversation
+\nRollin' marijuana, that's a cheap vacation
+\nMy every day shit, my every day shit
+\nMy every night shit, my every day shit
+\nMy every day shit, my every day shit
+\nMy every day shit, my every night shit
 ` , 
     mand : 
 `昼夜不停地游览城市
-每个人都需要你
-不，你不能使所有人平等
-虽然你有美丽的家庭
-你甚至没有人对你诚实
-呼吸直到我蒸发
-我整个身体都看穿了
-手工运输
-我比大多数人都了解
-反正我也不信任他们
-你不能和他们违法
-快点，晚上安静
-射手杀死左右
-渡过最糟糕的夜晚
-如果我把钱弄对了
-你知道我不需要你
-我告诉你，
-我希望麻袋装满
-我他妈的，不，我他妈的
-当我得到它时花
-我不是想让你
-无法跟上对话
-没人能联系到你
-为什么你的眼睛好起来？
-你是不是从séance打电话给我的？
-你来自我前世
-希望你一切都好，布鲁
-我先去了这里
-总是喜欢头
-信号进出
-希望你一切都好，布鲁
-每个人都需要你
-每个人都需要你
-哦，娜妮娜妮
-这感觉像是一个quaalude
-我的身体没有睡眠
-我的身体不是bit子
-新的开始啊
-新的开始，唤醒akh
-太阳下​​山了
-是时候开始新的一天了，布鲁
-无法保持裁员
-知道要生存就需要钱
-每天晚上屎
-每天屎
-在夜班前将宝贝放下在家
-你知道我听不见那些花了整夜的时间
-那个昆巴耶屎
-想看必杀技，但不想死
-想要感到自己不行，你能过去吗？
-轮班后跟我他妈的
-认识他们，男孩们想见我崩溃了，拉屎
-筋疲力尽，拉屎，压力加大，拉屎
-每天都这样
-闭嘴，我不要你的谈话
-Rollin'大麻，那是一个便宜的假期
-我每天的屎，每天晚上的屎，我每天的屎
-我一整夜都准备为你整夜
-一整夜都在等你
-我会嗡嗡叫你，只要你在外面就让我知道
-我整晚，你整晚都在想念
-还记得一些美好的夜晚
-然后回头看我就对了
-每天晚上都乱搞
-每天修补夜晚
-在上帝上，你应该匹配它，那就是
-没有白色打火机，直到我他妈的我的28岁以上
-1998年，我家有那个Ac歌
-哦，传奇
-更换器中至少保留了六张光盘
-当Boswell和Percy活跃起来时
-这座城市的夫妇主教在大厦
-所有牧师
-传教士的百万富翁身份
-有时候我们只能在肖尼家吃饭
-“特里娜”袭击后，我不得不转校
-你在休斯顿等我的公寓
-当我没有地址时待在你身边
-当我没有床垫时，该死在你身上
-每天晚上都在努力使它远离德克萨斯州
-在我夜班前将宝贝放下在家，是的
-你知道我听不见夜宵，昆巴耶屎
-想看必杀技，但不想死
-想要感到自己不行，你能过去吗？
-轮班后跟我他妈的
-认识他们，男孩想见我崩溃
-看到我很沮丧，压力很大
-那只是每天的屎
-闭嘴，我不要你的谈话
-Rollin'大麻，那是一个便宜的假期
-我每天都屎，我每天都屎
-我每天晚上屎，我每天屎
-我每天都屎，我每天都屎
-我每天都屎，我每天晚上都屎`
+\n每个人都需要你
+\n不，你不能使所有人平等
+\n虽然你有美丽的家庭
+\n你甚至没有人对你诚实
+\n呼吸直到我蒸发
+\n我整个身体都看穿了
+\n手工运输
+\n我比大多数人都了解
+\n反正我也不信任他们
+\n你不能和他们违法
+\n快点，晚上安静
+\n射手杀死左右
+\n渡过最糟糕的夜晚
+\n如果我把钱弄对了
+\n你知道我不需要你
+\n我告诉你，
+\n我希望麻袋装满
+\n我他妈的，不，我他妈的
+\n当我得到它时花
+\n我不是想让你
+\n无法跟上对话
+\n没人能联系到你
+\n为什么你的眼睛好起来？
+\n你是不是从séance打电话给我的？
+\n你来自我前世
+\n希望你一切都好，布鲁
+\n我先去了这里
+\n总是喜欢头
+\n信号进出
+\n希望你一切都好，布鲁
+\n每个人都需要你
+\n每个人都需要你
+\n哦，娜妮娜妮
+\n这感觉像是一个quaalude
+\n我的身体没有睡眠
+\n我的身体不是bit子
+\n新的开始啊
+\n新的开始，唤醒akh
+\n太阳下​​山了
+\n是时候开始新的一天了，布鲁
+\n无法保持裁员
+\n知道要生存就需要钱
+\n每天晚上屎
+\n每天屎
+\n在夜班前将宝贝放下在家
+\n你知道我听不见那些花了整夜的时间
+\n那个昆巴耶屎
+\n想看必杀技，但不想死
+\n想要感到自己不行，你能过去吗？
+\n轮班后跟我他妈的
+\n认识他们，男孩们想见我崩溃了，拉屎
+\n筋疲力尽，拉屎，压力加大，拉屎
+\n每天都这样
+\n闭嘴，我不要你的谈话
+\nRollin'大麻，那是一个便宜的假期
+\n我每天的屎，每天晚上的屎，我每天的屎
+\n我一整夜都准备为你整夜
+\n一整夜都在等你
+\n我会嗡嗡叫你，只要你在外面就让我知道
+\n我整晚，你整晚都在想念
+\n还记得一些美好的夜晚
+\n然后回头看我就对了
+\n每天晚上都乱搞
+\n每天修补夜晚
+\n在上帝上，你应该匹配它，那就是
+\n没有白色打火机，直到我他妈的我的28岁以上
+\n1998年，我家有那个Ac歌
+\n哦，传奇
+\n更换器中至少保留了六张光盘
+\n当Boswell和Percy活跃起来时
+\n这座城市的夫妇主教在大厦
+\n所有牧师
+\n传教士的百万富翁身份
+\n有时候我们只能在肖尼家吃饭
+\n“特里娜”袭击后，我不得不转校
+\n你在休斯顿等我的公寓
+\n当我没有地址时待在你身边
+\n当我没有床垫时，该死在你身上
+\n每天晚上都在努力使它远离德克萨斯州
+\n在我夜班前将宝贝放下在家，是的
+\n你知道我听不见夜宵，昆巴耶屎
+\n想看必杀技，但不想死
+\n想要感到自己不行，你能过去吗？
+\n轮班后跟我他妈的
+\n认识他们，男孩想见我崩溃
+\n看到我很沮丧，压力很大
+\n那只是每天的屎
+\n闭嘴，我不要你的谈话
+\nRollin'大麻，那是一个便宜的假期
+\n我每天都屎，我每天都屎
+\n我每天晚上屎，我每天屎
+\n我每天都屎，我每天都屎
+\n我每天都屎，我每天晚上都屎`
 }];
 
 selectRandomSongs = () => {
@@ -403,10 +403,21 @@ selectRandomSongs = () => {
     return songs[Math.floor(Math.random() * (max - min + 1)) + min]; 
 }
 
+
+
 document.querySelector('.blink-top-button').addEventListener('click' , (e) => {
-    alert();
     let randomSong = selectRandomSongs();
     let audio = document.querySelector('audio');
-    let source = document.querySelector('source');
-    source.setAttribute('src' , randomSong.address);
+    audio.setAttribute('src', randomSong.address);
+
+
+    //injects lyrics to text boxes
+    const selector1 = document.querySelector(".ogLang").querySelector(".lyrics");
+    selector1.innerText="";
+    selector1.innerText=randomSong.eng;
+
+    const selector2 = document.querySelector(".translatedLang").querySelector(".lyrics");
+    selector2.innerText="";
+    selector2.innerText=randomSong.mand;
+
 });
